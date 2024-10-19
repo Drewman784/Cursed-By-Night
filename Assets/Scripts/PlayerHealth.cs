47b598fd87e69d448fe19b3f700033d6d68532b6
Drew <technoman1@icloud.com>
2024-10-19 11:43:12 -0400

    Player Damage & Health

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,12 +9,27 @@ public class PlayerHealth : MonoBehaviour
 
     [SerializeField] private float player_starthp;
     [SerializeField] public float player_hp;
+    [SerializeField] public float Zombie_Damage;
+    [SerializeField] public float Enemy_2_Damage;
 
     // Start is called before the first frame update
     void Start()
     {
         // Game Start HP
         player_hp = player_starthp;
+    }
+
+    //Varying damage the player takes from enemies
+    private void OnCollisionEnter(Collision other) 
+    {
+        if (other.gameObject.CompareTag("Zombie Enemy"))
+        {
+            player_hp -= Zombie_Damage;
+        }
+        if (other.gameObject.CompareTag("Enemy 2"))
+        {
+            player_hp -= Enemy_2_Damage;
+        }
     }
 
     public void Player_TakeDamage()
