20b211ac31bd8d244c794512d80f078d75389e57
caruiter <99144307+caruiter@users.noreply.github.com>
2024-10-30 21:45:21 -0400

    salvage and enemy spawning revisions, midterm playtest adjustments
    Coding/Implementation
    - changed salvage and enemy spawner scripts to spawn based off of gameobject positions rather than coded vector3s
    - enemies continue spawning during the night rather than just at the beginning
    - removed "ghost" navmesh surface
    - drafted eyeballmonster prefab using model + zombie prefab NOTE: not implemented
    
    Adjustments:
    - test trap prefab now deals 10 damage - takes two hits to kill enemy
    - enemies now drop 1 eyeball each
    - cycle length set to 90 seconds
    - added salvage spawning points to greyboxed locations
    - moved enemy spawning points
    - organized some assets

--- Assets/Scripts/Controllers/EnemySpawner.cs
+++ Assets/Scripts/Controllers/EnemySpawner.cs
@@ -11,11 +11,21 @@ public class EnemySpawner : MonoBehaviour
     //[SerializeField] GameObject tempEnemyHolder;
     // Start is called before the first frame update
 
+    [SerializeField] List<GameObject> enemySpawnLocations;
+    private List<Vector3> spawnpoints;
     private int daynum;
+
+    private bool day;
     void Start()
     {
      activeEnemies = new List<GameObject>();  
      daynum = 1; 
+     day = true;
+
+     spawnpoints = new List<Vector3>();
+     foreach(GameObject loc in enemySpawnLocations){
+        spawnpoints.Add(loc.transform.position);
+     }
     }
 
     // Update is called once per frame
@@ -25,6 +35,7 @@ public class EnemySpawner : MonoBehaviour
     }
 
     public void SpawnEnemies(){
+        day = false;
         /**
         for(int a = 0; a<num; a++){
             GameObject g = Instantiate(enemyPrefab);
@@ -44,44 +55,54 @@ public class EnemySpawner : MonoBehaviour
             switch(num)
             {
                 case 1: //spawnpoint on day 1
-                    g.transform.position = new Vector3(-8,-.5f,-25);
+                    g.transform.position = spawnpoints[0];
                     Debug.Log("placed?");
+                    Debug.Log("spot: " +spawnpoints[0]);
+                    Debug.Log("loc: " + g.transform.position);
                     break;
                 case 2: //spawn point on day 2
-                    g.transform.position = new Vector3(-26.5f,-.5f,-25);
+                    g.transform.position = spawnpoints[1];
                     break;
                 case 3: //etc
-                    g.transform.position = new Vector3(-43,-.5f,-.1f);
+                    g.transform.position = spawnpoints[2];
                     break;
                 case 4: 
-                    g.transform.position = new Vector3(18,-.5f,-.1f);
+                    g.transform.position = spawnpoints[3];
                     break;
                 case 5: 
-                    g.transform.position = new Vector3(18,-.5f,19f);
+                    g.transform.position = spawnpoints[4];
                     break;
                 case 6: 
-                    g.transform.position = new Vector3(-8.3f,-.5f,19f);
+                    g.transform.position = spawnpoints[5];
                     break;
                 case 7: 
-                    g.transform.position = new Vector3(-30,-.5f,19);
+                    g.transform.position = spawnpoints[6];
                     break;
             }
             num--;
         }
-        daynum++;
+
         //tempEnemyHolder.SetActive(true);
     }
 
     public void DeleteEnemies(){
+        day = true;
+
         foreach(GameObject g in activeEnemies){
             //activeEnemies.Remove(g);
             Destroy(g);
         }
         activeEnemies = new List<GameObject>();
+
+        daynum++;
         //tempEnemyHolder.SetActive(false);
     }
 
     public void RemoveEnemyFromActiveList(GameObject g){
         activeEnemies.Remove(g);
+
+        if(activeEnemies.Count == 0 & !day){ //if enemies are defeated but it's still night, spawn more
+            SpawnEnemies();
+        }
     }
 }
--- Assets/Scripts/Controllers/SalvageSpawner.cs
+++ Assets/Scripts/Controllers/SalvageSpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using UnityEngine;
 
@@ -11,27 +12,53 @@ public class SalvageSpawner : MonoBehaviour
 
     [SerializeField] GameObject salvagePrefab;
 
-    [SerializeField] Vector3 centerOfSpawnArea;
+    [SerializeField] List<GameObject> centerOfSpawnArea;
+    private List<Vector3> spawnPoints;
 
     // Start is called before the first frame update
 
-    public void EnterDayCycle(){ //day cycle begins, salvage spawns
-        int numToSpawn = 5;
-        for(int a =0; a<numToSpawn;a++){
-
-            GameObject n = Instantiate(salvagePrefab); //create salvage
-
-            Vector3 pos = Random.insideUnitCircle * 10; //place around spawn point
-            pos += centerOfSpawnArea;
-            n.transform.position = new Vector3(pos.x, .5f, pos.z);
-
-            activeSalvage.Add(n); // add to list
+    void Start(){
+        spawnPoints = new List<Vector3>();
+        foreach(GameObject s in centerOfSpawnArea){
+            spawnPoints.Add(s.gameObject.transform.position);
+            //Debug.Log("adding " +s );
         }
+    }
+
+    public void EnterDayCycle(){ //day cycle begins, salvage spawns
+        //Debug.Log("got here");
+        if(spawnPoints == null){ //patch job on intial day cycle causing errors
+            Patch();
+        }
+
+        int numToSpawn = 5;
+        foreach (Vector3 spot in spawnPoints){
+            for(int a =0; a<numToSpawn;a++){
+                //Debug.Log(spot+ " salvage spawned");
+
+                GameObject n = Instantiate(salvagePrefab); //create salvage
+
+                Vector3 pos = Random.insideUnitCircle * 10; //place around spawn point
+                 pos += spot;
+                 n.transform.position = new Vector3(pos.x, .5f, pos.z);
+
+                activeSalvage.Add(n); // add to list
+        }
+        }
+
     }
 
     public void EnterNightCycle(){ //night cycle begins, salvage despawns
         foreach( GameObject s in activeSalvage){
             Destroy(s);
+        }
+    }
+
+    private void Patch(){ //converts gameobjects to spawn locations vector3
+        spawnPoints = new List<Vector3>();
+        foreach(GameObject s in centerOfSpawnArea){
+            spawnPoints.Add(s.gameObject.transform.position);
+            Debug.Log("adding " +s );
         }
     }
 }
